7a36119fa62d96c281c9ebc62c806b926a2c4996
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-01-25 01:00:07 +0200

    Added the header component

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Shopping App",
@@ -14,9 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider dynamic>
-    <html lang="en">
-      <body> {children}</body>
-    </html>
+      <html lang="en">
+        <body>
+          <main>
+            <Header />
+            {children}</main>
+        </body>
+
+      </html>
     </ClerkProvider>
   );
 }
