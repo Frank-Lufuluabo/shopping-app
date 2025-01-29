bf1e327e2340594541037593fadc94a62d3d0b3c
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-01-29 03:07:20 +0200

    Added the product thumbnail component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,13 +1,27 @@
-import { Product } from "@/sanity.types";
+"use client";
 
+import { Product } from "@/sanity.types";
+import { AnimatePresence, motion } from "framer-motion";
+import ProductThub from "./ProductThub";
 
 function ProducGrid({ products }: { products: Product[] }) {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-        
-            {/* {products?.map((product) => {
-                return <ProductThub key={product._id} product={product} />
-            })} */}
+            {products?.map((product) => {
+                return (
+                    <AnimatePresence key={product._id}>
+                        <motion.div
+                            layout
+                            initial={{ opacity: 0.2 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="flex justify-center"
+                        >
+                            <ProductThub key={product._id} product={product} />
+                        </motion.div>
+                    </AnimatePresence>
+                );
+            })}
         </div>
     );
 }
