cc0b25159d5657449a7b21c5926887e0452fef66
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-02-05 23:58:05 +0200

    Implementing complet Add Basket functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,12 @@ function Header() {
                     <Link href="/basket"
                         className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded">
                         <TrolleyIcon className="w-6 h-6" />
-                        {/* Span item count once global state is implemented */}
+
+                        <span className="absolute -top-2 -right-2 bg-red-500 text-white
+                        rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                            {itemCount}
+                        </span>
+
                         <span>My Basket</span>
                     </Link>
 
